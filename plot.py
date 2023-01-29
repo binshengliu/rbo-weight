ac86f5c7490b0caa5ad97f72f0602116c15d3dcb
Binsheng Liu <441707+binshengliu@users.noreply.github.com>
2023-01-30 10:04:46 +1100

    add discrete weight table

--- plot.py
+++ plot.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from matplotlib.ticker import MultipleLocator
 
 from rbo_weight import rbo_cumulative_weight, rbo_discrete_weight
 
@@ -15,28 +16,30 @@ def plot_cumulative_weight() -> None:
             data.append([d, p, weight])
     df = pd.DataFrame(data, columns=["Rank", "RBO P", "Weight"])
     df2 = df.pivot(index="Rank", columns="RBO P", values="Weight")
-    print(df2.to_markdown())
+    print(df2.to_markdown("cumulative_weight.txt", floatfmt="f"))
     df["RBO P"] = df["RBO P"].astype(str)
     g = sns.relplot(kind="line", data=df, x="Rank", y="Weight", hue="RBO P", aspect=2.0)
     g.ax.set_xticks([1] + list(range(5, 101, 5)))
     g.ax.set_yticks(np.arange(0, 1.1, 0.1))
     g.tight_layout()
-    # g.savefig("rbo_cumulative_weight.pdf")
     g.savefig("cumulative.png")
 
 
 def plot_rank_weight() -> None:
     ps = [0.5, 0.8, 0.85, 0.9, 0.95, 0.99]
-    ds = np.arange(1, 21)
+    ds = np.arange(1, 101)
     data = []
     for p in ps:
         for d in ds:
             weight = rbo_discrete_weight(d, p)
             data.append([d, p, weight])
     df = pd.DataFrame(data, columns=["Rank", "RBO P", "Weight"])
+    df2 = df.pivot(index="Rank", columns="RBO P", values="Weight")
+    print(df2.to_markdown("discrete_weight.txt", floatfmt="f"))
     df["RBO P"] = df["RBO P"].astype(str)
+    df = df[df["Rank"]<=20]
     g = sns.relplot(kind="line", data=df, x="Rank", y="Weight", hue="RBO P", aspect=2.0)
-    g.ax.set_xticks(ds)
+    g.ax.xaxis.set_major_locator(MultipleLocator(1))
     g.tight_layout()
     g.savefig("discrete.png")
 
